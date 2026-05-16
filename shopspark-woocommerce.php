2a4abed94bd41538f0653571104dd7142201b822
Joan Manel Sancho Garcia <manelsancho1989@gmail.com>
2026-05-16 19:48:28 +0200

    chore: setup composer and psr-4 autoload for ShopSpark WooCommerce plugin

--- shopspark-woocommerce.php
+++ shopspark-woocommerce.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: ShopSpark WooCommerce Addons
  * Description: Custom WooCommerce features for ShopSpark theme
@@ -7,27 +8,7 @@
  */
 
 if (!defined('ABSPATH')) exit;
-
-// Autoload simple
-spl_autoload_register(function ($class) {
-
-    $prefix = 'ShopSpark\\Woo\\';
-    $base_dir = __DIR__ . '/inc/';
-
-    $len = strlen($prefix);
-
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-
-    $relative_class = substr($class, $len);
-
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+require_once __DIR__ . '/vendor/autoload.php';
 
 // Init plugin
 add_action('plugins_loaded', function () {
